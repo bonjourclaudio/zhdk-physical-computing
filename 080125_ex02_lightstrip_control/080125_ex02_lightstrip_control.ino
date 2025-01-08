7da410946efbe55d25fc69b0de3c4d3fd7782171
bonjourclaudio <claudio.weckherlin@gmail.com>
2025-01-08 15:50:18 +0100

    adapt color; add comments and fix driver

--- 080125_ex02_lightstrip_control/080125_ex02_lightstrip_control.ino
+++ 080125_ex02_lightstrip_control/080125_ex02_lightstrip_control.ino
@@ -4,6 +4,13 @@ int potPin = A0;
 int LEDPin = 6;
 int numOfPixels = 8;
 
+// Adapt 3rd parameter based on the LED strip. Options:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
 Adafruit_NeoPixel pixels(numOfPixels, LEDPin, NEO_GRBW + NEO_KHZ800);
 
 void setup() {
@@ -24,7 +31,7 @@ void loop() {
   int lightPixels = map(analogRead(potPin), 0, 1023, 0, 8);
 
   for(int i=0; i<= lightPixels; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0, 0));
+    pixels.setPixelColor(i, pixels.Color(0, 150, 150, 0));
 
     pixels.show(); 
   }
