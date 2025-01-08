2c6ab3457ee85d603f71a65391ab6b81cf6103ce
bonjourclaudio <claudio.weckherlin@gmail.com>
2025-01-08 15:25:18 +0100

    use correct driver for my adafruit

--- 080125_ex02_lightstrip_control/080125_ex02_lightstrip_control.ino
+++ 080125_ex02_lightstrip_control/080125_ex02_lightstrip_control.ino
@@ -4,7 +4,7 @@ int potPin = A0;
 int LEDPin = 6;
 int numOfPixels = 8;
 
-Adafruit_NeoPixel pixels(numOfPixels, LEDPin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(numOfPixels, LEDPin, NEO_GRBW + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
@@ -17,14 +17,14 @@ void setup() {
 
 void loop() {
   pixels.clear();
+  pixels.setBrightness(10);
 
   Serial.println(analogRead(potPin));
 
   int lightPixels = map(analogRead(potPin), 0, 1023, 0, 8);
 
-  for(int i=0; i<= lightPixels; i++) { // For each pixel...
-
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+  for(int i=0; i<= lightPixels; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0, 0));
 
     pixels.show(); 
   }
